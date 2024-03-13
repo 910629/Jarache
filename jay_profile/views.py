e6e14df1ff248a300da34fd580c1816b5abefa79
910629 <khunyelijarache@gmail.com>
2024-03-13 15:08:58 +0200

    Added doctring for 'index' function in 'jay_profile/views.py' file

--- jay_profile/views.py
+++ jay_profile/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    """This method renders the index page for the application.
+
+    :Args request: An HTTP request object.
+
+    :Returns: An HTTP response with the rendered index.html template.
+    """
     return render(request, 'index.html')
 
 
