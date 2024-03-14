cbd6b42ff389a61c2c22c38d46cc968e9a582ce1
910629 <khunyelijarache@gmail.com>
2024-03-14 08:36:44 +0200

    Added docstrings for 'Question' class & its '__str__' method in 'poll/models.py' file

--- poll/models.py
+++ poll/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    """This class defines a model for questions within the application.
+    A question is characterized by its question text (`question_text`)
+    and the date and time it was published (`pub_date`).
+
+    :param question_text (CharField): The text of the question (max length 200 characters).
+    :param pub_date (DateTimeField): The date and time the question was published.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-def __str__(self):
-    return self.question_text
+    def __str__(self):
+        """This method returns a string representation of the question object.
+
+        :returns: string representation of 'question_text' attribute
+
+        :rtype: str
+        """
+        return self.question_text
 
 
 class Choice(models.Model):
