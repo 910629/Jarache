2af45f2cd3076a4a31cf636cdcf829e00abb6b45
910629 <khunyelijarache@gmail.com>
2024-03-14 08:47:23 +0200

    Added docstrings for 'Choice' class & its '__str__' method in 'poll/models.py' file

--- poll/models.py
+++ poll/models.py
@@ -15,7 +15,7 @@ class Question(models.Model):
     def __str__(self):
         """This method returns a string representation of the question object.
 
-        :returns: string representation of 'question_text' attribute
+        :returns: string representation of 'question_text' attribute.
 
         :rtype: str
         """
@@ -23,9 +23,25 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    """This class defines a model for choices within the application.
+    A choice is associated with a specific question (`question`) through a
+    foreign key relationship. It has a `choice_text` field to store the text of 
+    the choice (max length 200 characters) and a `vote` field (IntegerField) 
+    to keep track of the number of votes received.
+
+    :Attribute question (ForeignKey): A foreign key relationship to the Question model.
+    :Attribute choice_text (CharField): The text of the choice (max length 200 characters).
+    :Attribute vote (IntegerField): The number of votes received by this choice (default 0).
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     vote = models.IntegerField(default=0)
 
     def __str__(self):
+        """This method is used by Django to represent the choice object as a string.
+
+        :returns: string representation of 'choice_text' attribute.
+
+        :rtype: str
+        """
         return self.choice_text
